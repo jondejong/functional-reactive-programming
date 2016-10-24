503eb8bb134d363442e4fda706cd8f7576c24f02
Jon DeJong <jon.dejong@gmail.com>
2016-10-23 20:33:24 -0500

    Adding functional example of looping demo

--- functional-demo/index.js
+++ functional-demo/index.js
@@ -1,78 +1,9 @@
 const fs = require('fs-extra');
 const path = require('path');
+const processor = require('./processor');
 
 const data = fs.readJsonSync(path.join(__dirname, '../data/zips-fixed.json'));
 
-const reducer = (currentState, zip) => {
+const output = path.join(__dirname, 'out/output.json')
 
-    if (!currentState[zip.state]) {
-        currentState[zip.state] = {
-            cities: {}
-        }
-    }
-
-    if (!currentState[zip.state].cities[zip.city]) {
-        currentState[zip.state].cities[zip.city] = 0
-    }
-
-    currentState[zip.state].cities[zip.city] += zip.pop
-    return currentState
-} 
-
-const stateMapper = state => {
-
-
-    const cities = Object.keys(state.cities).map(cityName => {
-        let population = state.cities[cityName]
-        const city = {
-            name: cityName, population: population
-        }
-    })
-    // state.cities = cities.sort((a, b) => {
-    //     if(a.population < b.population) {
-    //         return 1
-    //     } else if (b.population < a.population) {
-    //         return -1
-    //     }
-    //     return 0
-    // })
-    return {
-        state: state.
-        }
-
-}
-
-const reduction = data.reduce(reducer, {});
-
-const reductionMapper = stateName => {
-    return reduction[stateName]
-}
-
-const states = Object.keys(r).map(reductionMapper).map(stateMapper)
-
-
-// let final = []
-
-// Object.keys(states).forEach(stateName => {
-//     let finalState = {
-//         state: stateName
-//     }
-
-//     const state = states[stateName]
-//     const city = state.cities[0]
-//     finalState.city = city
-//     final.push(finalState)
-// })
-
-// final.sort((a, b) => {
-//     if(a.state < b.state) {
-//         return -1
-//     } else if(b.state < a.state) {
-//         return 1
-//     }
-//     return 0
-// })
-
-// const output = path.join(__dirname, 'out/output.json')
-
-// fs.writeJsonSync(output, final)
+fs.writeJsonSync(output, processor.process(data));
---
+++ functional-demo/processor.js
@@ -0,0 +1,75 @@
+let processor = {}
+
+processor.stateFilter = zip => {
+    return state => {
+        return state.state == zip.state
+    }
+}
+
+processor.cityFilter = zip => {
+    return city => {
+        return city.name == zip.city
+    }
+}
+
+processor.reducer = (currentState, zip) => {
+    let foundStates = currentState.filter(processor.stateFilter(zip))
+
+    if (foundStates.length < 1) {
+        const state = {
+            state: zip.state,
+            cities: []
+        }
+        currentState.push(state)
+        foundStates.push(state)
+    }
+    const state = foundStates[0]
+
+    const foundCities = state.cities.filter(processor.cityFilter(zip))
+    if (foundCities.length < 1) {
+        const city = {
+            name: zip.city,
+            population: 0
+        }
+        state.cities.push(city)
+        foundCities.push(city)
+    }
+    const city = foundCities[0]
+    city.population += zip.pop
+    return currentState
+}
+
+processor.cityComparator = (a, b) => {
+    if (a.population < b.population) {
+        return 1
+    } else if (b.population < a.population) {
+        return -1
+    }
+    return 0
+}
+
+processor.stateComparator = (a, b) => {
+    if (a.state < b.state) {
+        return -1
+    } else if (b.state < a.state) {
+        return 1
+    }
+    return 0
+}
+
+processor.stateMapper = state => {
+    state.cities = state.cities.sort(processor.cityComparator)
+    return {
+        state: state.state,
+        city: state.cities[0]
+    }
+}
+
+processor.process = data => {
+    return data.reduce(reducer, []).map(stateMapper).sort(processor.stateComparator);
+}
+
+module.exports = processor
+
+
+
